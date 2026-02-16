674a05a86b34543af755317e9fb75fa667e21d9c
Moni-999 <monishanaik73385@gmail.com>
2026-02-16 23:22:00 +0530

    2nd commit

--- pythoncode/ArrOperations/arrop1.py
+++ pythoncode/ArrOperations/arrop1.py
@@ -1,5 +1,5 @@
 import numpy as np
-arr= np.array([1, 2, 3], [4, 5, 6], [7, 8, 9])
+arr= np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 reshaped_arr=arr.reshape(1, 9)
 print("Original Array:")
 print(arr)
