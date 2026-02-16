44614de9e5811c920d000e611e4e9e4ec8277665
Moni-999 <monishanaik73385@gmail.com>
2026-02-16 23:20:12 +0530

    2nd commit

--- pythoncode/ArrOperations/arrop1.py
+++ pythoncode/ArrOperations/arrop1.py
@@ -1,8 +1,9 @@
 import numpy as np
-array1= np.array([1, 2, 3, 4])
-array2=np.array([10, 20, 30, 40]) 
+arr= np.array([1, 2, 3], [4, 5, 6], [7, 8, 9])
+reshaped_arr=arr.reshape(1, 9)
+print("Original Array:")
+print(arr)
+print("Reshaped Array (1, 9):")
+print(reshaped_arr)
 
-result=array1*array2
-print("Array 1:",array1)
-print("Array 2:", array2)
-print("Element-wise multiplication result:", result)
+
