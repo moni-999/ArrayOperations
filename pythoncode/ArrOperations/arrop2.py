b79525297fb7336745c983770d4bb75379654eeb
Moni-999 <monishanaik73385@gmail.com>
2026-02-16 23:15:22 +0530

    2nd commit

---
+++ pythoncode/ArrOperations/arrop2.py
@@ -0,0 +1,7 @@
+import numpy as np 
+array1 = np.array([1, 2, 3, 4])
+array2 = np.array([10, 20, 30, 40])
+result = array1 * array2
+print("Array 1:", array1)
+print("Array 2:", array2)
+print("Element-wise multiplication result:", result)
